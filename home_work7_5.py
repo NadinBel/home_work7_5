0b3076c71d265737e595fc440e2aabba27adce12
NadinBel <143278036+NadinBel@users.noreply.github.com>
2024-08-16 16:43:08 +0300

    Add files via upload

---
+++ home_work7_5.py
@@ -0,0 +1,11 @@
+import os
+import time
+for root, dirs, files in os.walk('.'):
+  for file in files:
+    filepath = os.path.join('.', file)
+    filetime = os.path.getmtime(file)
+    formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
+    filesize = os.stat(file).st_size
+    parent_dir = os.path.dirname(os.path.relpath(file, '..'))
+    print(
+      f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {filesize} байт, Время изменения: {formatted_time}, Родительская директория: {parent_dir}')
